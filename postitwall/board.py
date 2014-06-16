3d7deb5c134571f5ba893982e95d60455bed7f52
Aurélien Gâteau <agateau@kde.org>
2014-06-16 16:47:17 +0200

    When a board lists multiple projects, group by projects

--- postitwall/board.py
+++ postitwall/board.py
@@ -3,7 +3,7 @@ from __future__ import division, absolute_import, print_function, unicode_litera
 
 from datetime import datetime, timedelta
 
-from sqlobject import AND, LIKE
+from sqlobject import AND, OR, LIKE
 from sqlobject.sqlbuilder import LEFTJOINOn, IN
 
 from yokadi.core.db import Task, TaskKeyword, Project
@@ -17,32 +17,30 @@ def compute_min_date():
     return now - timedelta(now.weekday())
 
 
-def list_tasks(status, project_list, filters):
-    lst = Task.select(
-            AND(IN(Task.q.project, project_list),
-                Task.q.status == status,
-                *filters),
-            orderBy=Task.q.id, distinct=True,
-            join=LEFTJOINOn(Task, TaskKeyword, Task.q.id == TaskKeyword.q.taskID)
-        )
-    return list(lst)
-
-
 class Board(object):
+    __slots__ = ("name", "tasks")
     def __init__(self, name, filter_string):
         self.name = name
 
         project_name, keyword_filters = parseutils.extractKeywords(filter_string)
-        project_list = Project.select(LIKE(Project.q.name, project_name))
 
         q_filters = [x.filter() for x in keyword_filters]
+
+        project_list = Project.select(LIKE(Project.q.name, project_name))
+        q_filters.append(IN(Task.q.project, project_list))
 
         # Skip notes
         q_filters.append(parseutils.KeywordFilter("!@" + NOTE_KEYWORD).filter())
 
-        self.tasks = {}
-
-        self.tasks["new"] = list_tasks('new', project_list, q_filters)
-        self.tasks["started"] = list_tasks('started', project_list, q_filters)
+        # Only list done tasks if they were done after min_date
         min_date = compute_min_date()
-        self.tasks["done"] = list_tasks('done', project_list, q_filters + [Task.q.doneDate >= min_date])
+        q_filters.append(OR(
+            Task.q.status != 'done',
+            Task.q.doneDate >= min_date
+            ))
+
+        self.tasks = Task.select(
+            AND(*q_filters),
+            orderBy=Task.q.id, distinct=True,
+            join=LEFTJOINOn(Task, TaskKeyword, Task.q.id == TaskKeyword.q.taskID)
+        )
--- postitwall/view.py
+++ postitwall/view.py
@@ -1,6 +1,8 @@
 # Python 2/3 compatibility
 from __future__ import division, absolute_import, print_function, unicode_literals
 
+import itertools
+
 import markdown
 
 from flask import Flask, render_template, redirect
@@ -18,10 +20,33 @@ def md2html(value):
     return Markup(markdown.markdown(value, extensions=['urlize']))
 
 
+def sort_and_group(lst, key):
+    lst = sorted(lst, key=key)
+    return itertools.groupby(lst, key)
+
+
+def create_board_info(board):
+    class ProjectInfo(object):
+        def __init__(self, name, tasks):
+            self.name = name
+            tasks = sort_and_group(tasks, key=lambda x: x.status)
+            self.tasks = dict((k, list(v)) for k, v in tasks)
+
+    class BoardInfo(object):
+        def __init__(self, board):
+            self.name = board.name
+            lst = sort_and_group(board.tasks, key=lambda x: x.project.name)
+            self.project_info_list = [ProjectInfo(name, list(tasks)) for name, tasks in lst]
+
+    return BoardInfo(board)
+
+
 @app.route("/")
 def index():
-    board_lst = [Board(name, filters) for name, filters in settings.BOARDS]
-    return render_template("index.html", board_lst=board_lst)
+    board_list = [Board(name, filters) for name, filters in settings.BOARDS]
+    board_info_list = [create_board_info(x) for x in board_list]
+
+    return render_template("index.html", board_info_list=board_info_list)
 
 
 @app.route("/favicon.ico")
